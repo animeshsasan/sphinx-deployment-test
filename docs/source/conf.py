d53d1abf0488660bfa65fe1de6a6d3fc58a7af73
Animesh Sasan <sasananimesh@gmail.com>
2025-09-12 15:14:22 +0530

    Testing dev version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,7 +131,7 @@ html_theme_options = {
         "image_light": "logo_light.png",
         "image_dark": "logo_dark.png",
     },
-    "navbar_end": ["version-switcher", "navbar-icon-links"],
+    "navbar_end": ["version-switcher"],
     "footer_start": ["footer_start"],
     "footer_end": ["footer_end"],
 }
